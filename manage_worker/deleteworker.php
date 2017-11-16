5f1c3bc2c580527c8fa23bcb270c422d48bd60fc
Ardhi Surya <rdsurya147@gmail.com>
2017-11-16 10:51:59 +0800

    Delete worker checking function

--- manage_worker/deleteworker.php
+++ manage_worker/deleteworker.php
@@ -11,6 +11,21 @@ if (isset($_POST['workersid'])) {
     $type = isset($_POST['type']) ? $_POST['type'] : "";
 
     $Id = $_POST['workersid'];
+    
+//    check first whether any admin left. If no admin left, abort process
+    if(strcasecmp($type, "admin") == 0){
+        $sql="Select username from login where type='admin';";
+        $result = mysqli_query($conn, $sql);
+        if(mysqli_num_rows($result) < 3){
+            $reply->status = false;
+            $reply->msg = "You cannot delete this user because you will be left with no admin.";
+            
+            echo json_encode($reply);
+            mysqli_close($conn);
+            return false;
+           
+        }
+    }
     $sql = "DELETE FROM workers WHERE workersid='$Id'";
     if ($conn->query($sql) === true) {
         $reply->status = true;
